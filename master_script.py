e34def87e5885b59180896d1aa6577fb048bc17f
Rohit R. Mohanty <x23113057@student.ncirl.ie>
2023-12-09 15:23:34 +0000

    testing script files while passing arguments from stdin

---
+++ master_script.py
@@ -0,0 +1,8 @@
+import sys
+import test_script
+
+name = sys.argv[1]
+print("Hello", name)
+
+test_script.bye(name)
+
---
+++ test_script.py
@@ -0,0 +1,8 @@
+import sys
+
+def bye(name):
+    print("Bye", name)
+
+if __name__ == 'main':
+    bye(sys.argv[1])
+
